4881951a1ad20b8ab5863de95991806ef2b8538e
Necris45 <Necris01@mail.ru>
2024-12-16 09:49:17 +0500

    возникла проблема с celery

--- config.py
+++ config.py
@@ -17,8 +17,8 @@ def create_app() -> Flask:
     app = Flask(__name__)
     app.config.from_mapping(
         CELERY=dict(
-            broker_url="redis://127.0.0.1:6379",
-            result_backend="redis://127.0.0.1:6379",
+            broker_url="redis://redis:6379",
+            result_backend="redis://redis:6379",
             task_ignore_result=True,
         ),
     )
